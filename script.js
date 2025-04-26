6474f2ba1053bf04eddffbef41b4c56b8cb81a3d
wajiharashid <wajiharashidtms@gmail.com>
2025-04-26 12:33:44 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showSidebar(){
+    const Sidebar =document.querySelector('.sidebar')
+    Sidebar.style.display = 'flex '
+}
+function hideSidebar(){
+    const Sidebar =document.querySelector('.sidebar')
+    Sidebar.style.display = 'none'
+}
